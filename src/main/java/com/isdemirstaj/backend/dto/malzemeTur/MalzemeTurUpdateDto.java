4910e170a4298d2973bd89d6721a6a9bd9cd9d0b
Mehmet Abdurrahman Gök <155295812+mehmetabdurrahmangok@users.noreply.github.com>
2026-07-21 11:13:58 +0300

    malzeme tür tanımı ekleme ve güncelleme için giriş ayarı yapıldı

--- src/main/java/com/isdemirstaj/backend/dto/malzemeTur/MalzemeTurUpdateDto.java
+++ src/main/java/com/isdemirstaj/backend/dto/malzemeTur/MalzemeTurUpdateDto.java
@@ -1,9 +1,15 @@
 package com.isdemirstaj.backend.dto.malzemeTur;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class MalzemeTurUpdateDto {
+    @NotBlank(message = "Malzeme türü ismi boş bırakılamaz!!")
+    @Size(min = 2, max = 20, message = "malzeme tür ismi 2 ve 20 karakter arasında olmalı!!!")
+    @Pattern(regexp = "^[a-zA-ZçğıöşüÇĞİÖŞÜ\\s]+$", message = "Sadece Türkçe karakterler kullanılabilir!!")
     private String malzemeTurAdi;
     private String oper;
 }
