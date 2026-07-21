55f9fd2eab325fc4e021e68cc1e2d6a92b68fc45
Mehmet Abdurrahman Gök <155295812+mehmetabdurrahmangok@users.noreply.github.com>
2026-07-21 11:08:26 +0300

    malzeme ve tür ekleme işlemlerinde artık sadece Türkçe karakterler giriliebiliyor

--- src/main/java/com/isdemirstaj/backend/dto/malzeme/MalzemeCreateDto.java
+++ src/main/java/com/isdemirstaj/backend/dto/malzeme/MalzemeCreateDto.java
@@ -1,6 +1,9 @@
 package com.isdemirstaj.backend.dto.malzeme;
 
 import com.isdemirstaj.backend.entity.enums.MenseiEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.Getter;
@@ -10,6 +13,11 @@ import lombok.Getter;
 public class MalzemeCreateDto { // Malzeme entity'sinin oluşturulması için kullanılan DTO sınıfı
 
     private String malzemeKodu;
+
+    @NotBlank(message = "Malzeme adı boş bırakılamaz!!!")
+    @Size(min = 2, max = 50, message = "Malzeme adının uzunluğu 2 ve 50 karakter arasında olmalıdır.")
+    // Koruma kısmı
+    @Pattern(regexp = "^[a-zA-ZçğıöşüÇĞİÖŞÜ\\s]+$", message = "Sadece Türkçe karakterler kullanılabilir!!")
     private String malzemeAdi;
     private Long malzemeTurId;
     private MenseiEnum mensei;
--- src/main/java/com/isdemirstaj/backend/dto/malzeme/MalzemeUpdateDto.java
+++ src/main/java/com/isdemirstaj/backend/dto/malzeme/MalzemeUpdateDto.java
@@ -1,11 +1,18 @@
 package com.isdemirstaj.backend.dto.malzeme;
 
 import com.isdemirstaj.backend.entity.enums.MenseiEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class MalzemeUpdateDto {
     private String malzemeKodu;
+    @NotBlank(message = "Malzeme adı boş bırakılamaz!!!")
+    @Size(min = 2, max = 50, message = "Malzeme adının uzunluğu 2 ve 50 karakter arasında olmalıdır.")
+    @Pattern(regexp = "^[a-zA-ZçğıöşüÇĞİÖŞÜ\\s]+$", message = "Sadece Türkçe karakterler kullanılabilir!!")
     private String malzemeAdi;
     private Long malzemeTurId;
     private MenseiEnum mensei;
